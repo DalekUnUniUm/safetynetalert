e30acbac00ed342bfd4eafe15f6f5d561109a335
DaleUnuniUm <martin.romuald2@hotmail.fr>
2021-01-06 10:34:34 +0100

    versions 0.1

---
+++ src/main/java/com/openclassrooms/safetynetalert/SafetynetalertApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.safetynetalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetynetalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetynetalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/safetynetalert/SafetynetalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.safetynetalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
